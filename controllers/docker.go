0f36b9ed04eaafada2134f6b613fba6eafa18ec0
Seth Lawler <slawler@dewberry.com>
2023-03-06 09:36:03 -0500

    add docker jobs

---
+++ controllers/docker.go
@@ -0,0 +1,203 @@
+package controllers
+
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/mount"
+	volumetypes "github.com/docker/docker/api/types/volume"
+	"github.com/docker/docker/client"
+	"github.com/labstack/gommon/log"
+)
+
+type DockerController struct {
+	cli *client.Client
+}
+
+func NewDockerController() (*DockerController, error) {
+	c := new(DockerController)
+	var err error
+	c.cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
+// returns container id, error
+func (c *DockerController) ContainerRun(ctx context.Context, image string, command []string, volumes []VolumeMount, envVars map[string]string) (string, error) {
+	hostConfig := container.HostConfig{}
+	log.Debug("Initialize Container run")
+	//	hostConfig.Mounts = make([]mount.Mount,0);
+
+	var mounts []mount.Mount
+
+	for _, volume := range volumes {
+		mount := mount.Mount{
+			Type:   mount.TypeVolume,
+			Source: volume.Volume.Name,
+			Target: volume.HostPath,
+		}
+		mounts = append(mounts, mount)
+	}
+
+	hostConfig.Mounts = mounts
+	envs := make([]string, 0)
+	for k, v := range envVars {
+		envs = append(envs, k+"="+v)
+	}
+
+	resp, err := c.cli.ContainerCreate(ctx, &container.Config{
+		Tty:   true,
+		Image: image,
+		Cmd:   command,
+		Env:   envs,
+	}, &hostConfig, nil, nil, "")
+	// log.Info("Container Create response", resp)
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+
+	// log.Info("Start Container")
+	err = c.cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+
+	return resp.ID, nil
+}
+
+func (c *DockerController) Version() string {
+	return c.cli.ClientVersion()
+}
+
+// returns container logs as string, error
+func (c *DockerController) ContainerLog(ctx context.Context, id string) (string, error) {
+
+	reader, err := c.cli.ContainerLogs(ctx, id, types.ContainerLogsOptions{
+		ShowStdout: true,
+		ShowStderr: true})
+
+	if err != nil {
+		return "", err
+	}
+
+	buffer, err := io.ReadAll(reader)
+
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return string(buffer), nil
+}
+
+// returns container status code, error
+func (c *DockerController) ContainerWait(ctx context.Context, id string) (int64, error) {
+	resultC, errC := c.cli.ContainerWait(ctx, id, "")
+	select {
+	case err := <-errC:
+		return 0, err
+	case result := <-resultC:
+		return result.StatusCode, nil
+	}
+}
+
+func (c *DockerController) ContainerRemove(ctx context.Context, containerID string) error {
+	return c.cli.ContainerRemove(ctx, containerID, types.ContainerRemoveOptions{})
+}
+
+func (c *DockerController) ContainerKillAndRemove(ctx context.Context, containerID string, signal string) error {
+	err := c.cli.ContainerKill(ctx, containerID, signal)
+	if err != nil {
+		return err
+	}
+	return c.ContainerRemove(ctx, containerID)
+}
+
+// https://gist.github.com/miguelmota/4980b18d750fb3b1eb571c3e207b1b92
+func (c *DockerController) EnsureImage(ctx context.Context, image string, verbose bool) error {
+
+	reader, err := c.cli.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	var writer io.Writer
+	if verbose {
+		writer = os.Stdout
+	} else {
+		writer = io.Discard
+	}
+
+	_, err = io.Copy(writer, reader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type VolumeMount struct {
+	HostPath string
+	Volume   *volumetypes.Volume
+}
+
+func (c *DockerController) FindVolume(name string) (*volumetypes.Volume, error) {
+	volumes, err := c.cli.VolumeList(context.Background(), filters.NewArgs())
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range volumes.Volumes {
+		if v.Name == name {
+			return v, nil
+		}
+	}
+	return nil, nil
+}
+
+func (c *DockerController) EnsureVolume(name string) (*volumetypes.Volume, error) {
+	volume, err := c.FindVolume(name)
+	if err != nil {
+		return nil, err
+	}
+
+	if volume != nil {
+		return volume, nil
+	}
+
+	vol, err := c.cli.VolumeCreate(context.Background(), volumetypes.CreateOptions{
+		Driver: "local",
+		//		DriverOpts: map[string]string{},
+		//		Labels:     map[string]string{},
+		Name: name,
+	})
+
+	return &vol, err
+}
+
+func (c *DockerController) RemoveVolume(name string) error {
+	vol, err := c.FindVolume(name)
+	if err != nil {
+		return err
+	}
+
+	if vol == nil {
+		return nil
+	}
+
+	err = c.cli.VolumeRemove(context.Background(), name, true)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ jobs/docker-jobs.go
@@ -0,0 +1,193 @@
+package jobs
+
+import (
+	"app/controllers"
+	"context"
+	"fmt"
+	"time"
+	"unsafe"
+)
+
+type DockerJob struct {
+	Ctx         context.Context
+	CtxCancel   context.CancelFunc
+	UUID        string `json:"jobID"`
+	ContainerID string
+	ImgTag      string   `json:"imageAndTag"`
+	Cmd         []string `json:"commandOverride"`
+	UpdateTime  time.Time
+	Status      string `json:"status"`
+	MessageList []string
+	LogInfo     string
+	Links       []Link `json:"links"`
+}
+
+func (j *DockerJob) JobID() string {
+	return j.UUID
+}
+
+func (j *DockerJob) CMD() []string {
+	return j.Cmd
+}
+
+func (j *DockerJob) IMGTAG() string {
+	return j.ImgTag
+}
+
+func (j *DockerJob) JobLogs() string {
+	return j.LogInfo
+}
+
+func (j *DockerJob) Messages(includeErrors bool) []string {
+	return j.MessageList
+}
+
+func (j *DockerJob) NewMessage(m string) {
+	j.MessageList = append(j.MessageList, m)
+}
+
+func (j *DockerJob) NewErrorMessage(m string) {
+	j.MessageList = append(j.MessageList, m)
+	j.NewStatusUpdate(FAILED)
+	j.CtxCancel()
+}
+
+func (j *DockerJob) LastUpdate() time.Time {
+	return j.UpdateTime
+}
+
+func (j *DockerJob) NewStatusUpdate(s string) {
+	j.Status = s
+	j.UpdateTime = time.Now()
+}
+
+func (j *DockerJob) CurrentStatus() string {
+	return j.Status
+}
+
+func (j *DockerJob) ProviderID() string {
+	return j.ContainerID
+}
+
+func (j *DockerJob) Equals(job Job) bool {
+	switch jj := job.(type) {
+	case *DockerJob:
+		return j.Ctx == jj.Ctx
+	default:
+		return false
+	}
+}
+
+func (j *DockerJob) Create() error {
+	ctx, cancelFunc := context.WithCancel(j.Ctx)
+	j.Ctx = ctx
+	j.CtxCancel = cancelFunc
+
+	c, err := controllers.NewDockerController()
+	if err != nil {
+		j.NewErrorMessage(err.Error())
+		return err
+	}
+
+	// verify command in body
+	if j.Cmd == nil {
+		j.NewErrorMessage(err.Error())
+		return err
+	}
+
+	// pull image
+	err = c.EnsureImage(ctx, j.ImgTag, false)
+	if err != nil {
+		j.NewErrorMessage(err.Error())
+		return err
+	}
+
+	j.NewStatusUpdate(ACCEPTED)
+	return nil
+}
+
+func (j *DockerJob) Run() {
+	c, err := controllers.NewDockerController()
+	if err != nil {
+		j.NewErrorMessage(err.Error())
+		return
+	}
+
+	// start container
+	envVars := map[string]string{}
+	j.NewStatusUpdate(RUNNING)
+	containerID, err := c.ContainerRun(j.Ctx, j.ImgTag, j.Cmd, []controllers.VolumeMount{}, envVars)
+	if err != nil {
+		j.NewErrorMessage(err.Error())
+		return
+	}
+
+	j.ContainerID = containerID
+
+	// wait for process to finish
+	statusCode, err1 := c.ContainerWait(j.Ctx, j.ContainerID)
+	logs, err2 := c.ContainerLog(j.Ctx, j.ContainerID)
+	if err1 != nil {
+		j.NewErrorMessage(err1.Error())
+	} else if statusCode != 0 {
+		if err2 == nil {
+			j.LogInfo = logs
+		}
+		j.NewErrorMessage(fmt.Sprintf("container exi code: %d", statusCode))
+	} else if statusCode == 0 {
+		j.LogInfo = logs
+		j.NewStatusUpdate(SUCCESSFUL)
+	}
+
+	// clean up the finished job
+	err = c.ContainerRemove(j.Ctx, j.ContainerID)
+	if err != nil {
+		j.NewErrorMessage(err.Error())
+	}
+
+	j.CtxCancel()
+}
+
+// kill local container
+func (j *DockerJob) Kill() error {
+	c, err := controllers.NewDockerController()
+	if err != nil {
+		j.NewMessage(err.Error())
+	}
+
+	err = c.ContainerKillAndRemove(j.Ctx, j.ContainerID, "KILL")
+	if err != nil {
+		j.NewErrorMessage(err.Error())
+	}
+
+	j.NewStatusUpdate(DISMISSED)
+	j.CtxCancel()
+	return nil
+}
+
+func (j *DockerJob) GetSizeinCache() int {
+	cmdData := int(unsafe.Sizeof(j.Cmd))
+	for _, item := range j.Cmd {
+		cmdData += len(item)
+	}
+
+	messageData := int(unsafe.Sizeof(j.MessageList))
+	for _, item := range j.MessageList {
+		messageData += len(item)
+	}
+
+	// not calculated appropriately, add method...
+	linkData := int(unsafe.Sizeof(j.Links))
+
+	totalMemory := cmdData + messageData + linkData +
+		int(unsafe.Sizeof(j.Ctx)) +
+		int(unsafe.Sizeof(j.CtxCancel)) +
+		int(unsafe.Sizeof(j.UUID)) + len(j.UUID) +
+		int(unsafe.Sizeof(j.ContainerID)) + len(j.ContainerID) +
+		int(unsafe.Sizeof(j.ImgTag)) + len(j.ImgTag) +
+		int(unsafe.Sizeof(j.UpdateTime)) +
+		int(unsafe.Sizeof(j.Status)) +
+		int(unsafe.Sizeof(j.LogInfo)) + len(j.LogInfo)
+
+	return totalMemory
+}
