bd09df86c36879e75aa68aa1aae8d790f57648a7
Abdul Raheem Siddiqui <abdulraheemsiddiqui@gmail.com>
2023-09-01 13:52:21 -0400

    Use local images

---
+++ plugin-examples/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to build Docker image
+build_image() {
+    base_folder="$1"
+    sub_folder="$2"
+    dockerfile_path="$base_folder/$sub_folder/Dockerfile"
+
+    if [ -f "$dockerfile_path" ]; then
+        echo "Building $base_folder/$sub_folder..."
+        docker build -t "$base_folder-$sub_folder" "$base_folder/$sub_folder" &
+    fi
+}
+
+# Iterate over folders and initiate parallel builds
+for base_folder in */; do
+    base_folder="${base_folder%/}"  # Remove trailing slash
+    if [ "$base_folder" != "readme.md" ]; then
+        for sub_folder in "$base_folder"/*; do
+            sub_folder="$(basename $sub_folder)"
+            build_image "$base_folder" "$sub_folder"
+        done
+    fi
+done
+
+# Wait for all parallel jobs to finish
+wait
+
+echo "All builds complete."
